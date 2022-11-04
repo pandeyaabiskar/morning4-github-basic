3964016e1aa710fcdb6d539fc117f74fb1f967f4
nirtutac123 <nirutachapagain822@gmail.com>
2022-11-03 21:13:09 -0700

    I live in Lalitpur

--- niruta.js
+++ niruta.js
@@ -1,1 +1,16 @@
 console.log("NIeuta!")
+
+//git dash -  git clone link
+// 1. npm install
+//2.  git checkout -b nchapagain/add-file
+//3. git add .
+//4. git commit -m "My name is Niruta!"
+//5. git push
+//6. copy "git push --set-upstream origin nchapagain/add-file" 
+
+// git branch - D file name (delete any branch)
+// git checkout main
+
+//
+//
+//
